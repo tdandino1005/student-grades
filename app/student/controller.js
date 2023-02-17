4cbad1ea69bd07dad037884b12c1d5a5fb2359ca
Tony Dandino <tdandino1005@gmail.com>
2023-02-16 21:24:20 -0600

    Add student controller

---
+++ app/student/controller.js
@@ -0,0 +1,26 @@
+import initClient from "../client.js";
+import Student from "./Student.js";
+
+const controller = {
+  // Add a method to get all students
+  index() {
+    return Student.find();
+  },
+  show(id) {
+    return Student.findById(id);
+  },
+  // Add a method to get a single student by id
+  create(newStudent) {
+    return Student.create(newStudent);
+  },
+  // Add a method to create a new student
+};
+
+await initClient();
+
+controller
+  .index()
+  .then((students) => console.log(students))
+  .catch((err) => console.error(err.message));
+
+export default controller;
